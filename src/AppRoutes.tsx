51db67b866fab3aa5529f1ccb5804afe12dae04b
adrien <camepanta@hotmail.com>
2025-11-14 01:21:36 +0100

    overview page and item page updated. Bootstrap.working with db.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,6 +5,8 @@ import PrivateOutlet from './features/auth/PrivateOutlet';
 import Dashboard from './features/dashboard/Dashboard';
 import InventoryOverview from './features/inventory/InventoryOverview';
 import ImportExport from './features/importExport/ImportExport';
+import Overview from './features/overview/Overview';
+import ItemPage from './features/items/ItemPage';
 
 const AppRoutes = () => {
     return (
@@ -13,6 +15,8 @@ const AppRoutes = () => {
                 <Route path="" element={<Navigate to="/dashboard" replace />} />
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="importExport" element={<ImportExport />} />
+                <Route path="overview" element={<Overview />} />
+                <Route path="items/:itemId" element={<ItemPage />} />
                 <Route path="*" element={<PrivateOutlet />}>
                     <Route path="inventory" element={<InventoryOverview />} />
                 </Route>
