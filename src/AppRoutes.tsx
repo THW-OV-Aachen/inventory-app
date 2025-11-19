fca438165c0147a6dbd557591b65e2b1bcd36c33
Ewa Schönborn <ewa.schoenborn@gmail.com>
2025-11-19 21:17:17 +0100

    resolve merge conflicts

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,14 +4,9 @@ import App from './App';
 import PrivateOutlet from './features/auth/PrivateOutlet';
 import Dashboard from './features/dashboard/Dashboard';
 import InventoryOverview from './features/inventory/InventoryOverview';
-<<<<<<< HEAD
 import ImportExport from './features/importExport/ImportExport';
 import Overview from './features/overview/Overview';
 import ItemPage from './features/items/ItemPage';
-=======
-import ItemPage from './features/items/ItemPage';
-import Overview from './features/overview/Overview';
->>>>>>> origin/4-item-list-overview
 
 const AppRoutes = () => {
     return (
@@ -19,14 +14,9 @@ const AppRoutes = () => {
             <Route path="/" element={<App />}>
                 <Route path="" element={<Navigate to="/dashboard" replace />} />
                 <Route path="dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
                 <Route path="importExport" element={<ImportExport />} />
                 <Route path="overview" element={<Overview />} />
                 <Route path="items/:itemId" element={<ItemPage />} />
-=======
-                <Route path="items" element={<ItemPage />} />
-                <Route path="overview" element={<Overview />} />
->>>>>>> origin/4-item-list-overview
                 <Route path="*" element={<PrivateOutlet />}>
                     <Route path="inventory" element={<InventoryOverview />} />
                 </Route>
