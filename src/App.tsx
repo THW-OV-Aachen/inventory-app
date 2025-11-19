ae43f087a5c6bcfbd62ad931de5f6a911a891326
azizhammi <azizhammi0@gmail.com>
2025-11-19 13:32:31 +0100

    Integration project commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import MainLayout from './layout/MainLayout';
+import GlobalStyle from './styles/GlobalStyle';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; //bootstrap
+
+const App = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <MainLayout />
+        </>
+    );
+};
+
+export default App;
