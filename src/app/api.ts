43267fab81194c792cc8352e51ad0ce9bdc1f5cf
Ewa Schönborn <ewa.schoenborn@gmail.com>
2025-11-24 00:21:06 +0100

    added db functionalities

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,7 @@
 import { type IItem } from '../db/items';
 import { db } from '../db/db';
 import { useLiveQuery } from 'dexie-react-hooks';
+import { read } from 'fs';
 
 export type Scope = 'user' | 'editor' | 'admin';
 
@@ -18,6 +19,24 @@ export const inventoryApi = {
     useItems() {
         const items: IItem[] | undefined = useLiveQuery(() => db.items.orderBy('id').toArray(), []);
         return items ?? [];
+    },
+    async getItem(id: string | number): Promise<IItem | undefined> {
+        try {
+            const item = await db.items.get(id as any);
+            return item;
+        } catch (error) {
+            console.error('Failed to read item: ', error);
+            return undefined;
+        }
+    }
+    async updateItem(id: string | number, changes: Partial<IItem>): Promise<boolean> {
+        try {
+            await db.items.update(id as any, changes);
+            return true;
+        } catch (err) {
+            console.error('Failed to update item', err);
+            return false;
+        }
     },
     async deleteItem(id: string): Promise<void> {
         try {
