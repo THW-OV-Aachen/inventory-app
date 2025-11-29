125f3b41c6532204afe43829d40dad5d8d22298c
adrien <camepanta@hotmail.com>
2025-11-29 10:38:05 +0100

    modify and add item is added. Still in english, german version should come soon. Diverse test cases added, which can lead to popups, e.g. when adding an item, id and name are required. Also, when an identical id exists, then a popup will apear and the values need to be modified.

---
+++ AddItem.tsx
@@ -0,0 +1,16 @@
+tree HEAD:
+
+.gitignore
+.prettierrc
+.vscode/
+README.md
+eslint.config.js
+index.html
+package-lock.json
+package.json
+public/
+src/
+tsconfig.app.json
+tsconfig.json
+tsconfig.node.json
+vite.config.ts
